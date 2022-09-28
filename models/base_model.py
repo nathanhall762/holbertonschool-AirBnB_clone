484b2ba751e853ce5ccb4a2f11f7f55441e84a21
nathanhall762 <4943@holbertonstudents.com>
2022-09-28 14:14:49 -0500

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ Base Model Class """
+import uuid
 
 
-class Base():
+class BaseModel:
+    """ Base Class """
     def __init__(self):
+        self.id = str(uuid.uuid4())
+
         # Public instance attributes:
         # id: string - assign with an uuid when an instance is created:
             # you can use uuid.uuid4() to generate unique id but don’t forget to convert to a string
@@ -11,15 +15,22 @@ class Base():
         # created_at: datetime - assign with the current datetime when an instance is created
         # updated_at: datetime - assign with the current datetime when an instance is created and it will be updated every time you change your object
 
-    def __str__:
-        # __str__: should print: [<class name>] (<self.id>) <self.__dict__>
-    
-    # Public instance methods:
-        # save(self): updates the public instance attribute updated_at with the current datetime
-        # to_dict(self): returns a dictionary containing all keys/values of __dict__ of the instance:
-            # by using self.__dict__, only instance attributes set will be returned
-            # a key __class__ must be added to this dictionary with the class name of the object
-            # created_at and updated_at must be converted to string object in ISO format:
-                # format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)
-                # you can use isoformat() of datetime object
-            # This method will be the first piece of the serialization/deserialization process: create a dictionary representation with “simple object type” of our BaseModel
+    # __str__: should print: [<class name>] (<self.id>) <self.__dict__>
+    def __str__(self):
+        print("[{}] ({}) <{}>".format(__name__, self.id, self.__dict__))
+
+# Public instance methods:
+    # save(self): updates the public instance attribute updated_at with the current datetime
+    # def save(self):
+        
+
+    # to_dict(self): returns a dictionary containing all keys/values of __dict__ of the instance:
+    # def to_dict(self):
+        
+
+        # by using self.__dict__, only instance attributes set will be returned
+        # a key __class__ must be added to this dictionary with the class name of the object
+        # created_at and updated_at must be converted to string object in ISO format:
+            # format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)
+            # you can use isoformat() of datetime object
+        # This method will be the first piece of the serialization/deserialization process: create a dictionary representation with “simple object type” of our BaseModel
