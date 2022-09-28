9d8922bf89c2968f71f06f6fe1aa1689dd292539
nathanhall762 <4943@holbertonstudents.com>
2022-09-28 16:36:35 -0500

    removed comments

--- models/base_model.py
+++ models/base_model.py
@@ -23,8 +23,10 @@ class BaseModel:
     def to_dict(self):
         """ returns a dictionary containing all keys/values"""
 
-        return {
+        self.__dict__ = {
+            '__class__': __class__.__name__,
             'id': self.id,
             'created_at': self.created_at,
             'updated_at': self.updated_at,
         }
+        return self.__dict__
