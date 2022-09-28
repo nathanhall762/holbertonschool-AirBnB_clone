e4ddfb16b5ab7867c3d41a87caf20aef531cb925
nathanhall762 <4943@holbertonstudents.com>
2022-09-28 15:31:49 -0500

    fix: shorten updated_at

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now().isoformat()
-        self.updated_at = self.created_at
+        self.save()
 
     def __str__(self):
         return "[{}] ({}) <{}>".format(__name__, self.id, self.__dict__)
