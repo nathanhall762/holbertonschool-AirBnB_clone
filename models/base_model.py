3936749f1475471c1fc8a35faafced0a1b610bba
nathanhall762 <4943@holbertonstudents.com>
2022-09-28 17:37:45 -0500

    fix: changed datetime

--- models/base_model.py
+++ models/base_model.py
@@ -9,8 +9,8 @@ class BaseModel:
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().isoformat()
-        self.save()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) <{}>".format(__name__, self.id, self.__dict__)
@@ -18,10 +18,13 @@ class BaseModel:
     def save(self):
         """ sets updated_at to current datetime """
 
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values"""
 
-        self.__dict__['__class__'] = __class__.__name__
-        return self.__dict__
+        class_dict = self.__dict__.copy()
+        class_dict['__class__'] = __class__.__name__
+        class_dict['created_at'] = self.created_at.isoformat()
+        class_dict['updated_at'] = self.updated_at.isoformat()
+        return class_dict
