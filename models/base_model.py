b55a509bb18d727931787265157e8e349f7f4d77
nathanhall762 <4943@holbertonstudents.com>
2022-09-29 15:13:56 -0500

    add: task 5

--- models/base_model.py
+++ models/base_model.py
@@ -7,13 +7,25 @@ import uuid
 class BaseModel:
     """ Base Class """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key in kwargs:
+                if key == 'created_at':
+                    self.created_at = datetime.strptime(kwargs['created_at'],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                if key == 'created_at':
+                    self.updated_at = datetime.strptime(kwargs['updated_at'],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+
+        else:
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) <{}>".format(__name__, self.id, self.__dict__)
+        return "[{}] ({}) <{}>".format(BaseModel.__name__, self.id,
+                                       self.__dict__)
 
     def save(self):
         """ sets updated_at to current datetime """
