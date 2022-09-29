cc3d6115c9a482c2a8ccb8461c31b2946fcf9baa
conkobar <connorkleine@gmail.com>
2022-09-29 11:59:46 -0500

    trying different dict method

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """ Base Model Class """
 from datetime import *
 import uuid
+import json
 
 
 class BaseModel:
@@ -22,9 +23,15 @@ class BaseModel:
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values"""
+        # class_dict = self.__dict__.copy()
+        # class_dict['__class__'] = __class__.__name__
+        # class_dict['created_at'] = self.created_at.isoformat()
+        # class_dict['updated_at'] = self.updated_at.isoformat()
+        # return class_dict
 
-        class_dict = self.__dict__.copy()
-        class_dict['__class__'] = __class__.__name__
-        class_dict['created_at'] = self.created_at.isoformat()
-        class_dict['updated_at'] = self.updated_at.isoformat()
-        return class_dict
+        newDict = self.__dict__.copy()
+        newDict.pop((
+            key for key in dir(self)
+            if key.startswith("__") and key.endswith("__")), None)
+        newDict['__class__'] = __class__.__name__
+        return newDict
