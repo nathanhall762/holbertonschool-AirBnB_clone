3a3451b291707c0fcfa415874ea4b5e1705e796f
nathanhall762 <4943@holbertonstudents.com>
2022-09-30 15:44:25 -0500

    add: test_file_storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Test module for base model class."""
+import unittest
+from models.base_model import BaseModel
+
+
+class FileStorageTest(unittest.TestCase):
+    """Test for File Storage."""
+
+    def test_FileStorage(self):
+        """Test for File Storage."""
+        pass
