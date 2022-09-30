63792d70c7b59fdbc76c435114ef1337fe6e15d8
nathanhall762 <4943@holbertonstudents.com>
2022-09-30 15:13:38 -0500

    git reset

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """ Base Model Class """
-from datetime import *
+from datetime import datetime
 import uuid
+import models
 
 
 class BaseModel:
-    """ Base Class """
+    """ Base Class for HolBnB project """
 
     def __init__(self, *args, **kwargs):
+        """instantiates base class"""
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -18,27 +20,32 @@ class BaseModel:
                 if key == 'updated_at':
                     self.updated_at = datetime.strptime(kwargs['updated_at'],
                                                         "%Y-%m-%dT%H:%M:%S.%f")
-
         else:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
-        self.id = str(uuid.uuid4())
+            self.id = str(uuid.uuid4())
+            models.storage.new(self)
 
     def __str__(self):
+<<<<<<< HEAD
         return "[{}] ({}) {}".format(BaseModel.__name__, self.id,
                                        self.__dict__)
+=======
+        """overwriting ___str__ builtin"""
+        return "[{}] ({}) {}".format(BaseModel.__name__, self.id,
+                                     self.__dict__)
+>>>>>>> master
 
     def save(self):
         """ sets updated_at to current datetime """
-
         self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values"""
-
         class_dict = self.__dict__.copy()
-        class_dict['__class__'] = __class__.__name__
+        class_dict['__class__'] = self.__class__.__name__
         class_dict['created_at'] = self.created_at.isoformat()
         class_dict['updated_at'] = self.updated_at.isoformat()
         return class_dict
