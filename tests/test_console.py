9a7a54db1b67e2342420be55df83cbd40fbb3273
nathanhall762 <4943@holbertonstudents.com>
2022-10-03 15:53:18 -0500

    add: unittests

---
+++ tests/test_console.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Test module for console class."""
+import unittest
+
+
+class TestConsole(unittest.TestCase):
+    """Tests for console."""
+
+    def test_console(self):
+        """Test for console."""
+        pass
